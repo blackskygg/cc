cb9d1fc71f59c8ac3de3498ad59c6a453e72bbf9
Zhongze Liu <blackskygg@gmail.com>
2017-05-07 11:16:53 +0800

    f

--- parse/parse.go
+++ parse/parse.go
@@ -3,6 +3,7 @@ package parse
 import (
 	"regexp"
 	"strconv"
+	"strings"
 
 	govaluate "github.com/blackskygg/cc/third/govaluate_modified"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
@@ -10,13 +11,15 @@ import (
 
 func makeParameterMap(stub shim.ChaincodeStubInterface, exp, id string) (map[string]interface{}, error) {
 	result := make(map[string]interface{})
-	re, err := regexp.Compile(`id(_\w+)+`)
+	re, err := regexp.Compile(`a(_\w+)+`)
 	if err != nil {
 		return result, err
 	}
 
 	l := re.FindAllString(exp, -1)
 	for _, w := range l {
+		w = w[2:]
+		key := strings.Join(id, w)
 		re, err := stub.GetState(w)
 		val, err := strconv.Atoi(string(re))
 		if err != nil {
